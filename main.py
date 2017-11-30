acda36aca924b1e3132a6824064f410b85b3f82a
Jette Cantiller <jette.cantiller@C02T85SEH03Q.workdayinternal.com>
2017-11-30 14:58:49 -0800

    main client runner assuming exports are correct

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("We have no ideas!!!!")
+import os
+import time
+
+from slackclient import SlackClient
+
+ID = os.environ.get('BOT_NAME')
+TOKEN = os.environ.get('SLACK_TOKEN')
+
+slackclient = SlackClient(TOKEN)
+
+print ("LOLOLOLOL")

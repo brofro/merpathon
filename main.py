a1d5ef0a10469971e536c91888d80dc5f0b90928
Jette Cantiller <jette.cantiller@C02T85SEH03Q.workdayinternal.com>
2017-11-30 15:30:36 -0800

    adding parsing and listening

--- main.py
+++ main.py
@@ -3,9 +3,35 @@ import time
 
 from slackclient import SlackClient
 
-ID = os.environ.get('BOT_NAME')
-TOKEN = os.environ.get('SLACK_TOKEN')
+ID = ''
+TOKEN = ''
+AT_BOT = "<@" + ID + ">"
+COMMAND1 = "listen"
 
 slackclient = SlackClient(TOKEN)
 
-print ("LOLOLOLOL")
+def message_parser(message):
+    outlist = message
+    if outlist and len(outlist) > 0:
+        for out in outlist:
+            if out and 'text' in out and AT_BOT in out['text']:
+                return out['text'].split(AT_BOT)[1].strip().lower(), \
+                    out['channel']
+    return None, None
+
+def command_handler(command, channel):
+    response = "I'M MR MESEEKS LOOK AT ME, IDK WHAT YOU MEAN"
+    if command.startswith(COMMAND1):
+        response = "OKAY LET ME DO THAT"
+    slackclient.api_call("chat.postMessage", channel = channel, text = response, as_user=True)
+
+if __name__ == "__main__":
+    if slackclient.rtm_connect():
+        print ("RUNNING")
+        while True:
+            command, channel = message_parser(slackclient.rtm_read())
+            if command and channel:
+                command_handler(command, channel)
+            time.sleep(1)
+
+
